94d150f18f3e375e5922894c97efdf37ab8d93b1
AlexKiryushkin <sashakir94@mail.ru>
2020-05-09 19:41:01 +0700

    Reduction functions refactoring

--- SrmSolver/solver_reduction_functions.h
+++ SrmSolver/solver_reduction_functions.h
@@ -4,6 +4,7 @@
 
 #include "cuda_float_types.h"
 #include "delta_dirac_function.h"
+#include "float4_arithmetics.h"
 #include "gas_state.h"
 #include "math_utilities.h"
 
@@ -24,13 +25,11 @@ CudaFloatT<2U, ElemT> getMaxWaveSpeeds(const thrust::device_vector<GasStateT>& v
 {
   const auto first = thrust::make_transform_iterator(std::begin(values), kae::WaveSpeedXY{});
   const auto last = thrust::make_transform_iterator(std::end(values), kae::WaveSpeedXY{});
-
   return thrust::reduce(first, last, CudaFloatT<2U, ElemT>{ 0, 0 }, kae::ElemwiseMax{});
 }
 
 template <class GpuGridT, class GasStateT, class ElemT = typename GasStateT::ElemType>
 ElemT getDeltaT(const thrust::device_vector<GasStateT>& values,
-                const thrust::device_vector<ElemT>& currPhi,
                 ElemT courant)
 {
   CudaFloatT<2U, ElemT> lambdas = detail::getMaxWaveSpeeds(values);
@@ -40,117 +39,70 @@ ElemT getDeltaT(const thrust::device_vector<GasStateT>& values,
 template <class GasStateT, class ElemT = typename GasStateT::ElemType>
 CudaFloatT<4U, ElemT> getMaxEquationDerivatives(const thrust::device_vector<GasStateT>& prevValues,
                                                 const thrust::device_vector<GasStateT>& currValues,
-                                                const thrust::device_vector<ElemT>& currPhi,
                                                 ElemT dt)
 {
-  const auto prevFirst = thrust::make_transform_iterator(std::begin(prevValues), kae::ConservativeVariables{});
-  const auto prevLast = thrust::make_transform_iterator(std::end(prevValues), kae::ConservativeVariables{});
+  const auto zipFirst = thrust::make_zip_iterator(thrust::make_tuple(std::begin(prevValues), std::begin(currValues)));
+  const auto zipLast = thrust::make_zip_iterator(thrust::make_tuple(std::end(prevValues), std::end(currValues)));
 
-  const auto currFirst = thrust::make_transform_iterator(std::begin(currValues), kae::ConservativeVariables{});
-  const auto currLast = thrust::make_transform_iterator(std::end(currValues), kae::ConservativeVariables{});
-
-  const auto zipFirst = thrust::make_zip_iterator(thrust::make_tuple(prevFirst, currFirst, std::begin(currPhi)));
-  const auto zipLast = thrust::make_zip_iterator(thrust::make_tuple(prevLast, currLast, std::end(currPhi)));
-
-  const auto toDerivatives = [dt] __host__ __device__(
-    const thrust::tuple<CudaFloatT<4U, ElemT>, CudaFloatT<4U, ElemT>, ElemT> & conservativeVariables)
+  const auto toDerivatives = [] __device__(const thrust::tuple<GasStateT, GasStateT> & conservativeVariables)
   {
-    const auto prevVariable = thrust::get<0U>(conservativeVariables);
-    const auto currVariable = thrust::get<1U>(conservativeVariables);
-    const auto level = thrust::get<2U>(conservativeVariables);
-    if (level >= 0)
-      return CudaFloatT<4U, ElemT>{};
-
-    return CudaFloatT<4U, ElemT>{ (currVariable.x - prevVariable.x) / dt,
-      (currVariable.y - prevVariable.y) / dt,
-      (currVariable.z - prevVariable.z) / dt,
-      (currVariable.w - prevVariable.w) / dt };
+    const auto prevState = thrust::get<0U>(conservativeVariables);
+    const auto currState = thrust::get<1U>(conservativeVariables);
+    return ConservativeVariables::get(currState) - ConservativeVariables::get(prevState);
   };
 
-  const auto transformFirst = thrust::make_transform_iterator(zipFirst, toDerivatives);
-  const auto transformLast = thrust::make_transform_iterator(zipLast, toDerivatives);
-  return thrust::reduce(transformFirst, transformLast, CudaFloatT<4U, ElemT>{}, kae::ElemwiseAbsMax{});
+  return (1 / dt) * thrust::transform_reduce(zipFirst, zipLast, toDerivatives, CudaFloatT<4U, ElemT>{}, ElemwiseAbsMax{});
 }
 
 template <class GpuGridT, class ShapeT, class ElemT = typename GpuGridT::ElemType>
 ElemT getChamberVolume(const thrust::device_vector<ElemT> & currPhi)
 {
-  static thread_local thrust::device_vector<unsigned> indexVector = generateIndexMatrix<unsigned>(currPhi.size());
+  static thread_local auto indexVector = generateIndexMatrix<unsigned>(currPhi.size());
 
-  const auto tupleFirst = thrust::make_tuple(std::begin(indexVector), std::begin(currPhi));
-  const auto tupleLast = thrust::make_tuple(std::end(indexVector), std::end(currPhi));
+  const auto zipFirst = thrust::make_zip_iterator(thrust::make_tuple(std::begin(indexVector), std::begin(currPhi)));
+  const auto zipLast  = thrust::make_zip_iterator(thrust::make_tuple(std::end(indexVector), std::end(currPhi)));
 
-  const auto zipFirst = thrust::make_zip_iterator(tupleFirst);
-  const auto zipLast  = thrust::make_zip_iterator(tupleLast);
-
-  const auto toVolume = [] __host__ __device__(
-    const thrust::tuple<unsigned, ElemT> & tuple)
+  const auto toVolume = [] __device__ (const thrust::tuple<unsigned, ElemT> & tuple)
   {
-    const auto index     = thrust::get<0U>(tuple);
-    const auto i         = index % GpuGridT::nx;
-    const auto j         = index / GpuGridT::nx;
-    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny))
+    const auto i         = thrust::get<0U>(tuple) % GpuGridT::nx;
+    const auto j         = thrust::get<0U>(tuple) / GpuGridT::nx;
+    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny) || !ShapeT::isChamber(i * GpuGridT::hx, j * GpuGridT::hy))
     {
       return static_cast<ElemT>(0.0);
     }
 
-    const auto isChamber = ShapeT::isChamber(i * GpuGridT::hx, j * GpuGridT::hy);
-    if (!isChamber)
-    {
-      return static_cast<ElemT>(0.0);
-    }
-
-    const auto y = ShapeT::getRadius(i, j);
-    return ((thrust::get<1U>(tuple) > 0) ? 
-               static_cast<ElemT>(0.0) : 
-               2 * static_cast<ElemT>(M_PI) * y * GpuGridT::hx * GpuGridT::hy);
+    return ((thrust::get<1U>(tuple) > 0) ? static_cast<ElemT>(0.0) : 
+      2 * static_cast<ElemT>(M_PI) * ShapeT::getRadius(i, j) * GpuGridT::hx * GpuGridT::hy);
   };
 
-  const auto transformFirst = thrust::make_transform_iterator(zipFirst, toVolume);
-  const auto transformLast = thrust::make_transform_iterator(zipLast, toVolume);
-
-  return thrust::reduce(transformFirst, transformLast, static_cast<ElemT>(0.0));
+  return thrust::transform_reduce(zipFirst, zipLast, toVolume, static_cast<ElemT>(0.0), thrust::plus<ElemT>{});
 }
 
 template <class GpuGridT, class ShapeT, class GasStateT, class ElemT = typename GpuGridT::ElemType>
 ElemT getPressureIntegral(const thrust::device_vector<GasStateT>& gasValues,
                           const thrust::device_vector<ElemT>& currPhi)
 {
-  static thread_local thrust::device_vector<unsigned> indexVector = generateIndexMatrix<unsigned>(currPhi.size());
+  static thread_local auto indexVector = generateIndexMatrix<unsigned>(currPhi.size());
 
-  const auto tupleFirst = thrust::make_tuple(std::begin(gasValues), std::begin(indexVector), std::begin(currPhi));
-  const auto tupleLast = thrust::make_tuple(std::end(gasValues), std::end(indexVector), std::end(currPhi));
+  const auto zipFirst = thrust::make_zip_iterator(
+    thrust::make_tuple(std::begin(gasValues), std::begin(indexVector), std::begin(currPhi)));
+  const auto zipLast = thrust::make_zip_iterator(
+    thrust::make_tuple(std::end(gasValues), std::end(indexVector), std::end(currPhi)));
 
-  const auto zipFirst = thrust::make_zip_iterator(tupleFirst);
-  const auto zipLast = thrust::make_zip_iterator(tupleLast);
-
-  const auto toVolume = [] __host__ __device__(
-    const thrust::tuple<GasStateT, unsigned, ElemT> & tuple)
+  const auto toVolume = [] __device__ (const thrust::tuple<GasStateT, unsigned, ElemT> & tuple)
   {
-    const auto index = thrust::get<1U>(tuple);
-    const auto i = index % GpuGridT::nx;
-    const auto j = index / GpuGridT::nx;
-    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny))
+    const auto i = thrust::get<1U>(tuple) % GpuGridT::nx;
+    const auto j = thrust::get<1U>(tuple) / GpuGridT::nx;
+    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny) || !ShapeT::isChamber(i * GpuGridT::hx, j * GpuGridT::hy))
     {
       return static_cast<ElemT>(0.0);
     }
 
-    const auto isChamber = ShapeT::isChamber(i * GpuGridT::hx, j * GpuGridT::hy);
-    if (!isChamber)
-    {
-      return static_cast<ElemT>(0.0);
-    }
-
-    const auto y = ShapeT::getRadius(i, j);
-    return ((thrust::get<2U>(tuple) > 0) ? 
-              static_cast<ElemT>(0.0) : 
-              2 * static_cast<ElemT>(M_PI) * y * P::get(thrust::get<0U>(tuple)) * GpuGridT::hx * GpuGridT::hy);
+    return ((thrust::get<2U>(tuple) > 0) ? static_cast<ElemT>(0.0) : 
+      2 * static_cast<ElemT>(M_PI) * ShapeT::getRadius(i, j) * P::get(thrust::get<0U>(tuple)) * GpuGridT::hx * GpuGridT::hy);
   };
 
-  const auto transformFirst = thrust::make_transform_iterator(zipFirst, toVolume);
-  const auto transformLast = thrust::make_transform_iterator(zipLast, toVolume);
-
-  return thrust::reduce(transformFirst, transformLast, static_cast<ElemT>(0.0));
+  return thrust::transform_reduce(zipFirst, zipLast, toVolume, static_cast<ElemT>(0.0), thrust::plus<ElemT>{});
 }
 
 template <class GpuGridT, class ShapeT, class GasStateT, class ElemT = typename GpuGridT::ElemType>
@@ -166,35 +118,21 @@ ElemT getBurningSurface(const thrust::device_vector<ElemT>& currPhi,
 {
   static thread_local thrust::device_vector<unsigned> indexVector = generateIndexMatrix<unsigned>(currPhi.size());
 
-  const auto tupleFirst = thrust::make_tuple(std::begin(indexVector), 
-                                             std::begin(currPhi),
-                                             std::begin(normals));
-  const auto tupleLast = thrust::make_tuple(std::end(indexVector), 
-                                            std::end(currPhi),
-                                            std::end(normals));
+  const auto zipFirst = thrust::make_zip_iterator(
+    thrust::make_tuple(std::begin(indexVector), std::begin(currPhi), std::begin(normals)));
+  const auto zipLast = thrust::make_zip_iterator(
+    thrust::make_tuple(std::end(indexVector), std::end(currPhi), std::end(normals)));
 
-  const auto zipFirst = thrust::make_zip_iterator(tupleFirst);
-  const auto zipLast = thrust::make_zip_iterator(tupleLast);
-
-  const auto toVolume = [] __host__ __device__(
-    const thrust::tuple<unsigned, ElemT, CudaFloatT<2U, ElemT>> & tuple)
+  const auto toVolume = [] __device__(const thrust::tuple<unsigned, ElemT, CudaFloatT<2U, ElemT>> & tuple)
   {
-    const auto index   = thrust::get<0U>(tuple);
     const auto level   = thrust::get<1U>(tuple);
     const auto normals = thrust::get<2U>(tuple);
 
-    const auto i = index % GpuGridT::nx;
-    const auto j = index / GpuGridT::nx;
-    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny))
-    {
-      return static_cast<ElemT>(0.0);
-    }
-
+    const auto i = thrust::get<0U>(tuple) % GpuGridT::nx;
+    const auto j = thrust::get<0U>(tuple) / GpuGridT::nx;
     const auto xSurface = i * GpuGridT::hx - level * normals.x;
     const auto ySurface = j * GpuGridT::hy - level * normals.y;
-
-    const auto isBurningSurface = ShapeT::isBurningSurface(xSurface, ySurface);
-    if (!isBurningSurface)
+    if ((i >= GpuGridT::nx) || (j >= GpuGridT::ny) || !ShapeT::isBurningSurface(xSurface, ySurface))
     {
       return static_cast<ElemT>(0.0);
     }
@@ -203,10 +141,7 @@ ElemT getBurningSurface(const thrust::device_vector<ElemT>& currPhi,
     return 2 * static_cast<ElemT>(M_PI) * y * deltaDiracFunction(level, GpuGridT::hx) * GpuGridT::hx * GpuGridT::hy;
   };
 
-  const auto transformFirst = thrust::make_transform_iterator(zipFirst, toVolume);
-  const auto transformLast = thrust::make_transform_iterator(zipLast, toVolume);
-
-  return thrust::reduce(transformFirst, transformLast, static_cast<ElemT>(0.0));
+  return thrust::transform_reduce(zipFirst, zipLast, toVolume, static_cast<ElemT>(0.0), thrust::plus<ElemT>{});
 }
 
 template <class GpuGridT,
