d419522dceddf1eb224ea2f6373a9a420978fc9e
AlexKiryushkin <sashakir94@mail.ru>
2020-06-21 12:07:01 +0700

    Changed nozzleless shape parameter values

--- SrmSolver/shape_solver_types.h
+++ SrmSolver/shape_solver_types.h
@@ -18,9 +18,9 @@ template<class ElemT>
 struct ShapeSolverTypes<EShapeType::eNozzleLessShape, ElemT>
 {
   constexpr static unsigned nx{ 1344U + 1U };
-  constexpr static unsigned ny{ 128U + 1U };
+  constexpr static unsigned ny{ 150U + 1U };
   using LxToType            = std::ratio<1344, 1000>;
-  using LyToType            = std::ratio<128, 1000>;
+  using LyToType            = std::ratio<150, 1000>;
   using GpuGridType         = GpuGrid<nx, ny, LxToType, LyToType, 3U, ElemT>;
   using ShapeType           = kae::SrmShapeNozzleLess<GpuGridType>;
   constexpr static ElemT hx = GpuGridType::hx;
@@ -29,12 +29,12 @@ struct ShapeSolverTypes<EShapeType::eNozzleLessShape, ElemT>
   static_assert(stepsAreSame, "Grid steps are different!");
 
   using NuType                   = std::ratio<41, 100>;
-  using MtType                   = std::ratio<-3137331, 100000000>;
-  using TBurnType                = std::ratio<1, 1>;
-  using RhoPType                 = std::ratio<2167846, 1000>;
-  using P0Type                   = std::ratio<1, 10>;
+  using MtType                   = std::ratio<-96446, 1000000>;
+  using TBurnType                = std::ratio<2950, 1>;
+  using RhoPType                 = std::ratio<1700, 1>;
+  using P0Type                   = std::ratio<101325, 1>;
   using KappaType                = std::ratio<123, 100>;
-  using CpType                   = std::ratio<411, 100>;
+  using CpType                   = std::ratio<1800, 1>;
   using PropellantPropertiesType = PropellantProperties<NuType, MtType, TBurnType, RhoPType, P0Type, KappaType, CpType, ShapeType>;
 
   using GasStateType = GasState<PropellantPropertiesType, ElemT>;
@@ -45,14 +45,14 @@ struct ShapeSolverTypes<EShapeType::eNozzleLessShape, ElemT>
   constexpr static GasStateType initialGasState{ static_cast<ElemT>(1.0),
                                                  static_cast<ElemT>(0.0),
                                                  static_cast<ElemT>(0.0),
-                                                 detail::ToFloatV<P0Type, ElemT> };
+                                                 PropellantPropertiesType::P0 };
 };
 
 template<class ElemT>
 struct ShapeSolverTypes<EShapeType::eWithUmbrellaShape, ElemT>
 {
-  constexpr static unsigned nx{ 1640U + 1U };
-  constexpr static unsigned ny{ 600U + 1U };
+  constexpr static unsigned nx{ 820U * 2U + 1U };
+  constexpr static unsigned ny{ 300U * 2U + 1U };
   using LxToType = std::ratio<3280, 1000>;
   using LyToType = std::ratio<1200, 1000>;
   using GpuGridType = GpuGrid<nx, ny, LxToType, LyToType, 3U, ElemT>;
