e8d406e3da9c577c716e8c3a5441cff7ce0a810b
AlexKiryushkin <sashakir94@mail.ru>
2020-06-21 12:09:44 +0700

    Updated nozzleless shape trait class

--- SrmSolver/srm_shape_nozzle_less.h
+++ SrmSolver/srm_shape_nozzle_less.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#pragma warning(push, 0)
+#include <boost/geometry.hpp>
+#pragma warning(pop)
+
 #include "cuda_includes.h"
 
 #include "boundary_condition.h"
@@ -23,11 +27,23 @@ public:
 
   __host__ __device__ static ElemType getRadius(unsigned i, unsigned j);
 
+  constexpr __host__ __device__ static ElemType getInitialSBurn();
+
+  constexpr __host__ __device__ static ElemType getFCritical();
+
+  __host__ __device__ static ElemType isChamber(ElemType x, ElemType y);
+
+  __host__ __device__ static ElemType isBurningSurface(ElemType x, ElemType y);
+  
   const thrust::host_vector<ElemType> & values() const;
 
 private:
 
-  constexpr static unsigned offsetPoints = 20;
+  using Point2d      = boost::geometry::model::d2::point_xy<ElemType>;
+  using Polygon2d    = boost::geometry::model::polygon<Point2d>;
+  using Linestring2d = boost::geometry::model::linestring<Point2d>;
+
+  constexpr static unsigned offsetPoints = 32;
 
   constexpr static ElemType xLeft            = (offsetPoints + static_cast<ElemType>(0.5)) * GpuGridT::hx;
   constexpr static ElemType delta            = static_cast<ElemType>(0.01);
@@ -36,9 +52,42 @@ private:
   constexpr static ElemType yBottom          = (offsetPoints + static_cast<ElemType>(0.5)) * GpuGridT::hy;
   constexpr static ElemType Rk               = static_cast<ElemType>(0.1);
   constexpr static ElemType rkr              = static_cast<ElemType>(0.0245);
+
+  constexpr static unsigned nPoints              = 16U;
+  constexpr static unsigned dim                  = 2U;
+  constexpr static unsigned startPropellantPoint = 3U;
+  constexpr static unsigned endPropellantPoint   = 13U;
+  constexpr static ElemType points[nPoints][dim] = {
+    { static_cast<ElemType>(0.0),          static_cast<ElemType>(0.0) },
+    { static_cast<ElemType>(0.0),          Rk },
+    { delta,                               Rk },
+    { delta,                               static_cast<ElemType>(0.04) },
+    { delta + static_cast<ElemType>(0.01), static_cast<ElemType>(0.031) },
+    { static_cast<ElemType>(0.102),        static_cast<ElemType>(0.031) },
+    { static_cast<ElemType>(0.105),        rkr },
+    { static_cast<ElemType>(0.145),        rkr },
+    { static_cast<ElemType>(0.691),        static_cast<ElemType>(0.026) },
+    { static_cast<ElemType>(1.049),        static_cast<ElemType>(0.03) },
+    { static_cast<ElemType>(1.087),        static_cast<ElemType>(0.03) },
+    { static_cast<ElemType>(1.149),        static_cast<ElemType>(0.03) },
+    { static_cast<ElemType>(1.194),        static_cast<ElemType>(0.044) },
+    { static_cast<ElemType>(1.274),        static_cast<ElemType>(0.069) },
+    { static_cast<ElemType>(1.274),        static_cast<ElemType>(0.0) },
+    { static_cast<ElemType>(0.0),          static_cast<ElemType>(0.0) }
+  };
+
+  template <unsigned idx>
+  constexpr __host__ __device__ static ElemType initialSBurnPart()
+  {
+    constexpr auto dx = points[idx + 1U][0U] - points[idx][0U];
+    constexpr auto dy = points[idx + 1U][1U] - points[idx][1U];
+    constexpr auto distance = gcem::sqrt(dx * dx + dy * dy);
+    return static_cast<ElemType>(M_PI) * (points[idx + 1U][1U] + points[idx][1U]) * distance;
+  }
 private:
 
   thrust::host_vector<ElemType> m_distances;
+  Linestring2d m_linestring;
 };
 
 } // namespace kae
--- SrmSolver/srm_shape_nozzle_less_def.h
+++ SrmSolver/srm_shape_nozzle_less_def.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#pragma warning(push, 0)
-#include <boost/geometry.hpp>
-#pragma warning(pop)
-
 namespace kae {
+
 
 template <class GpuGridT>
 EBoundaryCondition SrmShapeNozzleLess<GpuGridT>::getBoundaryCondition(ElemType x, ElemType y)
@@ -30,40 +27,36 @@ auto SrmShapeNozzleLess<GpuGridT>::getRadius(unsigned i, unsigned j) -> ElemType
 
 template <class GpuGridT>
 SrmShapeNozzleLess<GpuGridT>::SrmShapeNozzleLess()
-  : m_distances{ GpuGridT::nx * GpuGridT::ny }
+  : m_distances{ GpuGridT::nx * GpuGridT::ny },
+    m_linestring{
+      { points[0U][0U],  points[0U][1U] },
+      { points[1U][0U],  points[1U][1U] },
+      { points[2U][0U],  points[2U][1U] },
+      { points[3U][0U],  points[3U][1U] },
+      { points[4U][0U],  points[4U][1U] },
+      { points[5U][0U],  points[5U][1U] },
+      { points[6U][0U],  points[6U][1U] },
+      { points[7U][0U],  points[7U][1U] },
+      { points[8U][0U],  points[8U][1U] },
+      { points[9U][0U],  points[9U][1U] },
+      { points[10U][0U], points[10U][1U] },
+      { points[11U][0U], points[11U][1U] },
+      { points[12U][0U], points[12U][1U] },
+      { points[13U][0U], points[13U][1U] },
+      { points[14U][0U], points[14U][1U] },
+      { points[15U][0U], points[15U][1U] }
+    }
 {
   namespace bg = boost::geometry;
-  using Point2d      = bg::model::d2::point_xy<ElemType>;
-  using Polygon2d    = bg::model::polygon<Point2d>;
-  using Linestring2d = bg::model::linestring<Point2d>;
 
-  Linestring2d linestring{
-    { static_cast<ElemType>(0.0),          static_cast<ElemType>(0.0) },
-    { static_cast<ElemType>(0.0),          Rk },
-    { delta,                               Rk },
-    { delta,                               static_cast<ElemType>(0.04) },
-    { delta + static_cast<ElemType>(0.01), static_cast<ElemType>(0.031) },
-    { static_cast<ElemType>(0.102),        static_cast<ElemType>(0.031) },
-    { static_cast<ElemType>(0.105),        rkr },
-    { static_cast<ElemType>(0.145),        rkr },
-    { static_cast<ElemType>(0.691),        static_cast<ElemType>(0.026) },
-    { static_cast<ElemType>(1.049),        static_cast<ElemType>(0.03) },
-    { static_cast<ElemType>(1.087),        static_cast<ElemType>(0.03) },
-    { static_cast<ElemType>(1.149),        static_cast<ElemType>(0.03) },
-    { static_cast<ElemType>(1.194),        static_cast<ElemType>(0.044) },
-    { static_cast<ElemType>(1.274),        static_cast<ElemType>(0.069) },
-    { static_cast<ElemType>(1.274),        static_cast<ElemType>(0.0) },
-    { static_cast<ElemType>(0.0),          static_cast<ElemType>(0.0) }
-  };
-
-  std::for_each(std::begin(linestring), std::end(linestring), [](auto & point)
+  std::for_each(std::begin(m_linestring), std::end(m_linestring), [](auto & point)
   {
     bg::set<0>(point, bg::get<0>(point) + xLeft);
     bg::set<1>(point, bg::get<1>(point) + yBottom);
   });
 
   Polygon2d polygon;
-  std::copy(std::begin(linestring), std::end(linestring), std::back_inserter(polygon.outer()));
+  std::copy(std::begin(m_linestring), std::end(m_linestring), std::back_inserter(polygon.outer()));
 
   for (unsigned i = 0U; i < GpuGridT::nx; ++i)
   {
@@ -72,13 +65,52 @@ SrmShapeNozzleLess<GpuGridT>::SrmShapeNozzleLess()
     {
       const auto y = j * GpuGridT::hy;
       const Point2d point{ x, y };
-      const auto distance = static_cast<ElemType>(bg::distance(point, linestring));
+      const auto distance = static_cast<ElemType>(bg::distance(point, m_linestring));
       const auto isInside = bg::covered_by(point, polygon);
 
       const auto index = j * GpuGridT::nx + i;
       m_distances[index] = isInside ? -std::fabs(distance) : std::fabs(distance);
     }
   }
+}
+
+template <class GpuGridT>
+constexpr auto SrmShapeNozzleLess<GpuGridT>::getInitialSBurn() -> ElemType
+{
+  ElemType initialSBurn{};
+  initialSBurn += initialSBurnPart<3U>();
+  initialSBurn += initialSBurnPart<4U>();
+  initialSBurn += initialSBurnPart<5U>();
+  initialSBurn += initialSBurnPart<6U>();
+  initialSBurn += initialSBurnPart<7U>();
+  initialSBurn += initialSBurnPart<8U>();
+  initialSBurn += initialSBurnPart<9U>();
+  initialSBurn += initialSBurnPart<10U>();
+  initialSBurn += initialSBurnPart<11U>();
+  initialSBurn += initialSBurnPart<12U>();
+  return initialSBurn;
+}
+
+template <class GpuGridT>
+constexpr auto SrmShapeNozzleLess<GpuGridT>::getFCritical() -> ElemType
+{
+  return static_cast<ElemType>(M_PI) * rkr * rkr;
+}
+
+template <class GpuGridT>
+__host__ __device__ auto SrmShapeNozzleLess<GpuGridT>::isChamber(ElemType x, ElemType y) -> ElemType
+{
+  return (xRight - x >= static_cast<ElemType>(0.1) * GpuGridT::hx) &&
+         (x - xLeft >= static_cast<ElemType>(0.1) * GpuGridT::hx);
+}
+
+template <class GpuGridT>
+__host__ __device__ auto SrmShapeNozzleLess<GpuGridT>::isBurningSurface(ElemType x, ElemType y) -> ElemType
+{
+  return (xRight - x >= static_cast<ElemType>(0.1) * GpuGridT::hx) &&
+         (x - xStartPropellant >= static_cast<ElemType>(0.1) * GpuGridT::hx) &&
+         (y - yBottom >= GpuGridT::hx) && 
+         (y - yBottom < Rk);
 }
 
 template <class GpuGridT>
@@ -93,8 +125,8 @@ bool SrmShapeNozzleLess<GpuGridT>::isPointOnGrain(ElemType x, ElemType y)
 {
   return (xRight - x >= static_cast<ElemType>(0.1) * GpuGridT::hx) &&
          (x - xStartPropellant >= static_cast<ElemType>(0.1) * GpuGridT::hx) &&
-         (y - yBottom >= GpuGridT::hx) && 
-         (y - yBottom <= Rk);
+         (y - yBottom >= rkr - GpuGridT::hx) &&
+         (y - yBottom < Rk);
 }
 
 template <class GpuGridT>
