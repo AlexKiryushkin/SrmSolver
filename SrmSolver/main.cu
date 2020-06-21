b953331662da563598973522c8a4b2d09a08bea1
AlexKiryushkin <sashakir94@mail.ru>
2020-06-21 12:35:45 +0700

    Build fixes

--- SrmSolver/main.cu
+++ SrmSolver/main.cu
@@ -9,18 +9,18 @@ int main()
 {
   try
   {
-    using ElemType = float;
-    using ShapeSolverType = kae::ShapeSolverTypes<kae::EShapeType::eWithUmbrellaShape, ElemType>;
-    using SrmSolverType   = ShapeSolverType::SrmSolverType;
-    using PropellantPropertiesType = ShapeSolverType::PropellantPropertiesType;
+    using ElemType               = float;
+    using ShapeSolverType        = kae::ShapeSolverTypes<kae::EShapeType::eNozzleLessShape, ElemType>;
+    using ShapeType              = ShapeSolverType::ShapeType;
+    using SrmSolverType          = ShapeSolverType::SrmSolverType;
+    using PhysicalPropertiesType = ShapeSolverType::PhysicalPropertiesType;
 
     const std::wstring writeFolder{ L"data" };
     const std::wstring currentPath = kae::append(kae::current_path(), writeFolder);
     kae::WriteToFolderCallback callback{ currentPath };
 
-    constexpr ElemType maximumMeanPressure{ static_cast<ElemType>(2) };
-    SrmSolverType srmSolver{ {}, ShapeSolverType::initialGasState, 100U, static_cast<ElemType>(0.9) };
-    srmSolver.quasiStationaryDynamicIntegrate(1000U, maximumMeanPressure, kae::ETimeDiscretizationOrder::eTwo, callback);
+    SrmSolverType srmSolver{ {}, ShapeSolverType::initialGasState, 100U, static_cast<ElemType>(0.8) };
+    srmSolver.dynamicIntegrate(1000U, 5.0f, kae::ETimeDiscretizationOrder::eTwo, callback);
   }
   catch (const std::exception & e)
   {
